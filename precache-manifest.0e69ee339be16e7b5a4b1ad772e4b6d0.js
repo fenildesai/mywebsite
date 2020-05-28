1720f4ba4d8e22a9986d513e8536bf2dfa088f92
Fenil Desai <feniludesai@yahoo.com>
2020-05-28 15:49:53 +0100

    Updates

---
+++ precache-manifest.0e69ee339be16e7b5a4b1ad772e4b6d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "511968a9457342c31c6c3c045be9b02c",
+    "url": "/mywebsite/index.html"
+  },
+  {
+    "revision": "9c12b0e3cb8b6bda851b",
+    "url": "/mywebsite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5b0f6a5f15d9c667598f",
+    "url": "/mywebsite/static/js/2.b3920cc1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mywebsite/static/js/2.b3920cc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c12b0e3cb8b6bda851b",
+    "url": "/mywebsite/static/js/main.e9973138.chunk.js"
+  },
+  {
+    "revision": "b89f8465f960a809d3fa",
+    "url": "/mywebsite/static/js/runtime-main.abec6fae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mywebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
